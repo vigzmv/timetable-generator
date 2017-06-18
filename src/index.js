35632b71a5dfcaab5cf3e2a7203d4cb2deaa8661
vigzmv <vigzmv@outlook.com>
2017-06-18 11:54:28 +0530

    Hello World

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<div> Hello World! </div>, document.getElementById('root'));
+registerServiceWorker();
