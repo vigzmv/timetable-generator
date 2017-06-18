fe64245d6077ff706c8db37ee86f41e6f3627323
vigzmv <vigzmv@outlook.com>
2017-06-18 22:35:10 +0530

    flowtypes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<div> Hello World! </div>, document.getElementById('root'));
+ReactDOM.render(
+  <div> Hello World!</div>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
